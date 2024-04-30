041dde67b0a158967fd7d44603a07636b770e383
SayfiddinMirkamolov <sayfiddinmirkamolov7@gmail.com>
2024-04-30 09:32:38 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,202 @@
+//for 1
+
+
+//  let a= 1 ;
+//  let b= 10;
+
+//  for(let i=a; i<=b ; i++){
+// 	console.log(Math.floor(i));
+//  }
+
+
+//for 2
+
+
+//  let a= 1 ;
+//  let b= 10;
+
+//  for(let i=b; i>a ; i--){
+// 	console.log(Math.floor(i));
+//  }
+
+// for 3
+
+
+//  let narxi = 5000 ;
+//  let kilo= 12;
+
+//  for (let i= 1; i<=kilo; i++){
+	
+// 	console.log(i , "kg konfetni narxi " , narxi*i );
+	
+
+//  }
+
+
+// for 4
+
+//  let narxi = 5000 ;
+//  let kilo= 2;
+ 
+
+//  for ( let i=1 ; i<kilo; i+=0.2){
+	
+// 	console.log(i , "kg konfetni narxi " , narxi*i );
+	
+
+//  }
+
+
+//for 5  
+
+
+//  let a= 1 ;
+//  let b= 10;
+//  let yigindi = 0;
+
+
+//  for(let i=a; i<=b ; i++){
+// 	console.log(i);
+// 	yigindi+=i;
+	
+//  }console.log(yigindi);
+
+
+//for 6 
+
+//  let a= 1 ;
+//  let b= 10;
+//  let kopaytma = 1;
+
+
+//  for(let i=a; i<=b ; i++){
+// 	console.log(i);
+// 	kopaytma*=i;
+	
+//  }console.log(kopaytma);
+
+//for 7
+
+//  let a= 1 ;
+//  let b= 3;
+//  let yigindi = 0;
+//  let kvadrat =0 ;
+
+
+//  for(let i=a; i<=b ; i++){
+// 	console.log(i);
+// 	kvadrat = i**2;
+// 	yigindi+=kvadrat;
+	
+//  }console.log(yigindi);
+
+
+//for 8
+
+// let n= 2
+
+// let s=0;
+// let yigindi = 0;
+ 
+// for(let i =1 ; i<=n ;i++ ){
+// 	s=1/i
+// 	yigindi+=s
+
+// }console.log(yigindi);
+
+
+// for 9
+
+
+// let  n = 2.1
+
+// let s = 0 
+
+// for(let i = 1 ; i<=n; i+=0.1){
+// 	s*=i
+	
+// }console.log(s);
+
+
+// for 10
+
+// let  n = 5;
+// let s=0;
+
+// for(let i=1 ; i <=(2*n-1) ; i+=2){
+// 	s+=i
+// console.log(s);
+// }
+
+
+
+// wihile 1
+
+// let a = 5;
+// let b = 4
+
+// while(a>b){
+
+// 	a-=b
+// 	console.log(a);
+// }
+
+// while 2
+
+// let a = 5
+// let  b=  3
+// let sana= 0
+// while(a>b){
+
+// 	a-=b
+// 	sana++;
+
+	
+// }console.log(sana);
+
+
+//while  3
+
+// let  son = 9
+
+// let  daraja = 1
+
+// while(son>daraja){
+// 	daraja*=3
+// }
+// if(son==daraja){
+// 	console.log("3 ning  darajasi ");
+// }
+
+// else{
+// 	console.log("3 ning darajsi emas");
+// }
+
+//while 4
+
+// let  n = 5
+// let  m = 2
+
+// let sana = 0;
+
+// while(n>m){
+// 	n-=m
+// 	sana++
+
+
+// }
+
+// 	console.log("butun  qismi  " , sana)
+// console.log("qoldiq qismi  " , n);
+
+//while 5 
+
+
+// let n =456;
+// let teskari =0;
+// while (n > 0) {
+// 		let qoldiq = n % 10;
+// 		teskari = teskari * 10 + qoldiq;
+// 		n = Math.floor(n / 10);console.log(qoldiq);
+// }
+// console.log(teskari);
